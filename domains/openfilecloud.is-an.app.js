c13db6fc08ea0ca0b97b16653a453b9865afb799
Ekin İlter Varlı <ekinnos@tutanota.com>
2024-02-01 15:47:19 +0300

    Create openfilecloud.is-an.app.js

---
+++ domains/openfilecloud.is-an.app.js
@@ -0,0 +1,13 @@
+addSubDomain({
+  description: 'Start sharing files quickly without wasting time',
+  domain: 'is-an.app',
+  subdomain: 'openfilecloud',
+  owner: {
+    repo: 'https://github.com/eknvarli/open-file-cloud',
+    email: 'ekinnos@tutanota.com',
+  },
+  record: {
+    CNAME: 'ekinvarli.pythonanywhere.com',
+  },
+  proxy: false,
+})
